dd09308ed31a8e4dfa19563ed5a0b3bc7aaa9cb2
HimaniTotwani <85440558+HimaniTotwani@users.noreply.github.com>
2021-06-06 17:42:23 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>JavaScript and React.js </h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+          everything from scratch including JavaScript, React.js, HTML.
+        </p>
+      </div>
+      <div className="note">
+        <h1>JavaScript and React.js </h1>
+        <p>
+          This was an amazing bootcamp taken by Shaurya Sinha. We covered
+          everything from scratch including JavaScript, React.js, HTML.
+        </p>
+      </div>
+      <div className="note">
+        <h1>JavaScript and React.js </h1>
+        <p>
+          This was an amazing bootcamp taken by Shaurya Sinha. We covered
+          everything from scratch including JavaScript, React.js, HTML.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Note;
